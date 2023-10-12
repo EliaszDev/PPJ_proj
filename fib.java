bca7f5a6496fd804fee6fa43c928056877c70c8f
EliaszDev <eliasz.babic@gmail.com>
2023-10-13 00:27:13 +0200

    kocham fib

---
+++ fib.java
@@ -0,0 +1,15 @@
+/**
+ * fib
+ */
+public class fib {
+    public static void main(String[] args) {
+        System.out.println("fib");
+        int n=10;
+        int f;
+        for(int i=1; i<n;i++){
+            
+        }
+
+    }
+ 
+}
